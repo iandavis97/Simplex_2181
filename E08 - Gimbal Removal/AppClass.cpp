93b6b7a65f7301d2229381beaf04c25aa2e4e6b8
iandavis97 <iandavis97@gmail.com>
2018-10-09 11:37:21 -0400

    E08 solution

--- E08 - Gimbal Removal/AppClass.cpp
+++ E08 - Gimbal Removal/AppClass.cpp
@@ -25,9 +25,11 @@ void Application::Display(void)
 	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
 	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
 
-	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
+	m_qOrientation=glm::angleAxis(glm::radians(1.0f),vector3(glm::radians(m_v3Rotation.x), glm::radians(m_v3Rotation.y), glm::radians(m_v3Rotation.z)));
+	m_m4Model = glm::rotate(IDENTITY_M4, glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f)*m_qOrientation);
+	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f)*m_qOrientation);
+	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f)*m_qOrientation);
+
 	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
 
 	//m_qOrientation = m_qOrientation * glm::angleAxis(glm::radians(1.0f), vector3(1.0f));
